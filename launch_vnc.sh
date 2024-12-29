c1dd2a80e404a8c17afe1bc8b892356a9af3e721
Quân Vũ Minh 💀 (Python, C++, Lua) <182662520+Quanvm0501alt1@users.noreply.github.com>
2024-12-29 09:49:21 +0700

    Add files

---
+++ launch_vnc.sh
@@ -0,0 +1,8 @@
+# sudo apt update && sudo apt install tigervnc-standalone-server qemu-kvm qemu-system-x86 openbox firefox neofetch kitty
+# git clone https://github.com/novnc/noVNC.git
+cd noVNC/
+sudo vncserver -SecurityTypes none -xstartup "openbox" -rfbport 5080
+echo "Wait for 3 sec before start VNC client..."
+sleep 3
+sudo ./utils/novnc_proxy --vnc 127.0.0.1:5080 --listen localhost:8080
+cd ../
