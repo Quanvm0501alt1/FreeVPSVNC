c19ffa310054302189d91db9cfd349273db7567c
Quân Vũ Minh 💀 (Python, C++, Lua) <quanvm0501alt1@tutamail.com>
2025-07-11 06:50:13 +0700

    Update launch_vnc.sh

--- launch_vnc.sh
+++ launch_vnc.sh
@@ -21,26 +21,26 @@ touch /root/.vnc/passwd
 chmod 600 /root/.vnc/passwd # Important for security
 # Ensure the .vnc directory exists for the root user
 mkdir -p /root/.vnc
-
+# Fast command curl -sSf https://raw.githubusercontent.com/Quanvm0501alt1/FreeVPSVNC/refs/heads/main/entrypoint.sh | sh -s run
 # Create a minimal xstartup script for the VNC server
 # This script tells VNC what desktop environment/window manager to launch
-cat << EOF > /root/.vnc/xstartup
-#!/bin/bash
-# Load X resources (optional)
-[ -r \$HOME/.Xresources ] && xrdb \$HOME/.Xresources
-# Start vncconfig to allow VNC client configuration
-vncconfig -iconic &
-# Start your desired desktop environment or window manager here
-# For example, to start Openbox and Tint2:
-openbox &
-tint2 &
-EOF
-# Make the xstartup script executable
-chmod +x /root/.vnc/xstartup
+# cat << EOF > /root/.vnc/xstartup
+# #!/bin/bash
+# # Load X resources (optional)
+# [ -r \$HOME/.Xresources ] && xrdb \$HOME/.Xresources
+# # Start vncconfig to allow VNC client configuration
+# vncconfig -iconic &
+# # Start your desired desktop environment or window manager here
+# # For example, to start Openbox and Tint2:
+# openbox &
+# tint2 &
+# EOF
+# # Make the xstartup script executable
+# chmod +x /root/.vnc/xstartup
 
-cd noVNC/
-sudo vncserver -SecurityTypes none -rfbport 5080
-echo "Wait for 3 sec before start VNC client..."
-sleep 3
-sudo ./utils/novnc_proxy --vnc 127.0.0.1:5080 --listen localhost:8080
-cd ../
+# cd noVNC/
+# sudo vncserver -SecurityTypes none -rfbport 5080
+# echo "Wait for 3 sec before start VNC client..."
+# sleep 3
+# sudo ./utils/novnc_proxy --vnc 127.0.0.1:5080 --listen localhost:8080
+# cd ../
