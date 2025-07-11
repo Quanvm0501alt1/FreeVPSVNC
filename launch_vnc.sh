1b5d7bf092c05121f72bf0f40b264a60ed836ed0
Quân Vũ Minh 💀 (Python, C++, Lua) <quanvm0501alt1@tutamail.com>
2025-07-11 07:07:58 +0700

    Update launch_vnc.sh

--- launch_vnc.sh
+++ launch_vnc.sh
@@ -8,6 +8,14 @@
 # sudo apt update && sudo apt install curl wget build-essential cmake make firefox kitty fluxbox openbox tigervnc-standalone-server git xterm websockify qemu-kvm qemu-system-x86 tint2 -y
 # hoặc / or
 # apt update && apt install sudo curl wget build-essential cmake make firefox-esr kitty fluxbox openbox tigervnc-standalone-server git xterm websockify qemu-kvm qemu-system-x86 tint2 -y
+# Chrome install
+# wget -O- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg > /dev/null
+# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# sudo apt update / apt update
+# Cách cài: / Ways to install
+# sudo apt update && sudo apt install curl wget build-essential cmake make google-chrome-stable kitty fluxbox openbox tigervnc-standalone-server git xterm websockify qemu-kvm qemu-system-x86 tint2 -y
+# Hoặc / or:
+# apt update && apt install curl wget build-essential cmake make google-chrome-stable kitty fluxbox openbox tigervnc-standalone-server git xterm websockify qemu-kvm qemu-system-x86 tint2 -y
 # git clone https://github.com/novnc/noVNC.git
 printf "#!/bin/sh\nunset SESSION_MANAGER\nunset DBUS_SESSION_BUS_ADDRESS\nexec fluxbox" > ~/.vnc/xstartup
 printf "#!/bin/sh\nunset SESSION_MANAGER\nunset DBUS_SESSION_BUS_ADDRESS\nexec fluxbox" > /root/.vnc/xstartup
